e8e3a7b1953f0c470383c15f5f378fb017da9cdd
vera.perez <vera.perez@upb.edu.co>
2022-12-02 05:47:52 -0500

    corazón

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("el computador tiene sueño v5 \n");
+        printf("lo que te dicte tu corazón \n");
+    return (EXIT_SUCCESS);
+}
+
