ef4af97ec694ab025305bea8ce004def46a8bd93
vera.perez <zasulich1@gmail.com>
2023-01-23 04:31:00 -0500

    retomando en enero23

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(){
     printf("el computador tiene sueño v5 \n");
     printf("lo que te dicte tu corazón \n");
     printf("hoy está rindiendo mucho");
-        return (EXIT_SUCCESS);
+    printf("retomando en enero 23")
+      return (EXIT_SUCCESS);
 }
 
